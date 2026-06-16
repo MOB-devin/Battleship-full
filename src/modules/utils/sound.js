fb59492674ba266838660a866be520e6055fca2a
MOB-devin <itsmichaelobrien@gmail.com>
2026-06-15 23:15:30 -0400

    Update sound.js

--- src/modules/utils/sound.js
+++ src/modules/utils/sound.js
@@ -12,13 +12,13 @@ const Sound = (() => {
     setTimeout(() => {
       const audio = new Audio(hitAudioSrc)
       audio.play().catch(() => {})
-    }, 750)
+    }, 250)
   }
   function miss() {
     setTimeout(() => {
       const audio = new Audio(missAudioSrc)
       audio.play().catch(() => {})
-    }, 750)
+    }, 250)
   }
 
   // LOAD BACKGROUND AUDIO ASYNCHRONOUSLY
